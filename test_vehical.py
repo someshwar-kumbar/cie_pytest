4d2f55441e061d1a6057c64b600e6ba08117685c
someshwar-kumbar <tncxsomu@gmail.com>
2025-12-27 13:13:01 +0530

    Update test_vehical.py

--- test_vehical.py
+++ test_vehical.py
@@ -2,9 +2,9 @@ import pytest
 from vehical import vehical_info
 def test_vhical_info():
     output=(
-        "pid=1234"
-        "bname=bmw"
-        "price=15000000"
-        "yop=2025"
+        "pid:1234"
+        "bname:bmw"
+        "price:15000000"
+        "yop:2025"
     )
     assert vehical_info(1234,"bmw",15000000,2025)==output
