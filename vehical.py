3cdf1f8b8a935610e9c52b5732f6c537d9816adb
someshwar-kumbar <tncxsomu@gmail.com>
2025-12-27 12:41:02 +0530

    Update vehical.py

--- vehical.py
+++ vehical.py
@@ -7,4 +7,4 @@ def vehical_info(pid,bname,price,yop):
     )
     return result
 if __name__=="__main__":
-    print(vehical_info(1234,"bmw",15000000,2025)
+    print(vehical_info(1234,"bmw",15000000,2025))
